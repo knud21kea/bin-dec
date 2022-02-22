78609819530a6ab4d8deef3e22bfce0cc551c27b
knud21kea <grah0070@stud.kea.dk>
2022-02-22 22:12:33 +0100

    Uses javascript function to convert

---
+++ src/main/java/com/example/bindecimal/BinDecimalApplication.java
@@ -0,0 +1,15 @@
+package com.example.bindecimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinDecimalApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(BinDecimalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bindecimal/BinDecimalApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.bindecimal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinDecimalApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
